e4a06749e0965a48044d6146e51d8ee14a9c164a
kidae92 <kidae92@hanmail.net>
2022-07-12 14:44:22 +0900

    회원 도메인 가입,조회 기본 틀

---
+++ src/main/java/spring/basic/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package spring.basic.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); // 동시성 문제가 발생할 수 있음 -> ConcurrentHashmap 사용
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
